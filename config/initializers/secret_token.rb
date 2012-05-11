81cd7735d990f4243c7cca05b49d8bec240a2a84
joan lee <joan.eun.lee@gmail.com>
2012-05-10 20:11:46 -0400

    init fear of commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'ada27d4583ff007d86b20d07c60b39ab4e9ba30b751450daffc9d663c29d5a868dab3ac0ae128a4259ff85ede538a35fae7231e99e439ef57d7fc79d8dbbd7e1'
